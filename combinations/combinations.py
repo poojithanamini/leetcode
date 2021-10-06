51cf53d0c37f4f1e95ac55ae7d8cb47836c6d4e5
Poojitha Namini <60817077+poojithanamini@users.noreply.github.com>
2021-10-06 20:59:16 +0530

    Time: 76 ms (96.99%), Space: 15.8 MB (49.96%) - LeetHub

---
+++ combinations/combinations.py
@@ -0,0 +1,6 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        l=[i for i in range(1,n+1)]
+        l1=list(map(list,itertools.combinations(l,k)))
+        return(l1)
+    
